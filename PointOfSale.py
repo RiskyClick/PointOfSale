f1317ed3e05c723a4329d241e6b46b0c432c81f2
RiskyClick <krich@mail.sfsu.edu>
2019-08-05 15:21:23 -0700

    still learning some of pygames lib

--- PointOfSale.py
+++ PointOfSale.py
@@ -3,25 +3,29 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1000, 750))
 done = False
-is_blue = True
+seats = True
 x = 30
 y = 50
 color = (0, 128, 255)
 clock = pygame.time.Clock()
+
+def mapOfBar():
+    seat = pygame.draw.circle(screen, color, (x, y), 25)
+
+def menu():
+    print("TESTTHE")
+    
 
 while not done:
         for event in pygame.event.get():
             print(pygame.mouse.get_pos())
             if event.type == pygame.QUIT:
                 one = True
-
-        while x < 1000:
-            
-            pygame.draw.circle(screen, color, (x, y), 25)
-            x += 75
-
-        if x > 1000:
-            x = 30
-            
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                seats = False
+                menu()
+                
+        if seats:
+            mapOfBar()
         
         pygame.display.flip()
