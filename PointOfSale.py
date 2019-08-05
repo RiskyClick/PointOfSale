219c7c7b64284ad5b90263eee6f5e061355ec55e
RiskyClick <krich@mail.sfsu.edu>
2019-08-05 15:18:29 -0700

    ;asuihdg

--- PointOfSale.py
+++ PointOfSale.py
@@ -1,31 +1,27 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((1000, 750))
 done = False
 is_blue = True
 x = 30
-y = 30
-
+y = 50
+color = (0, 128, 255)
 clock = pygame.time.Clock()
 
 while not done:
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                        is_blue = not is_blue
-        
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: y -= 3
-        if pressed[pygame.K_DOWN]: y += 3
-        if pressed[pygame.K_LEFT]: x -= 3
-        if pressed[pygame.K_RIGHT]: x += 3
-        
-        screen.fill((0, 0, 0))
-        if is_blue: color = (0, 128, 255)
-        else: color = (255, 100, 0)
-        pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+            print(pygame.mouse.get_pos())
+            if event.type == pygame.QUIT:
+                one = True
+
+        while x < 1000:
+            
+            pygame.draw.circle(screen, color, (x, y), 25)
+            x += 75
+
+        if x > 1000:
+            x = 30
+            
         
         pygame.display.flip()
-        clock.tick(120)
